eb9ae65bdd29f51079986bd683b719ba7785c20d
greg <greg.flanagan@locusenergy.com>
2014-03-20 10:57:04 -0700

    adding xml support back in

--- src/main/scala/net/liftmodule/restrecord/WebService.scala
+++ src/main/scala/net/liftmodule/restrecord/WebService.scala
@@ -39,10 +39,23 @@ trait RequestMakerInterface {
 }
 
 trait RequestHandlerInterface {
+  /** JSON Handlers */
   def find: (Request, FunctionHandler[JValue])
   def create(body: String): (Request, FunctionHandler[JValue])
   def save(body: String): (Request, FunctionHandler[JValue])
   def delete: (Request, FunctionHandler[JValue])
+
+  /** XML Handlers */
+  def findXML: (Request, FunctionHandler[NodeSeq])
+  def createXML(body: NodeSeq): (Request, FunctionHandler[NodeSeq])
+  def saveXML(body: NodeSeq): (Request, FunctionHandler[NodeSeq])
+  def deleteXML: (Request, FunctionHandler[NodeSeq])
+   
+  /** Form Handlers */
+  def createFORM(body: String): (Request, FunctionHandler[NodeSeq]) 
+
+  /* Download a file */
+  def download: (Request, FunctionHandler[Array[Byte]]) 
 }
 
 class WebServiceImpl(val request: Req) extends WebService with RequestHandler with RequestMaker
@@ -74,4 +87,19 @@ trait RequestHandler extends WebRequest with RequestHandlerInterface {
   def save(body: String) = request.PUT.setBody(body) > as.lift.Json
 
   def delete = request.DELETE > as.lift.Json
+
+
+  def findXML = request > as.xml.Elem 
+    
+  def saveXML(body: NodeSeq) = 
+    request.PUT.setBody(body.toString) > as.xml.Elem  
+
+  def createXML(body: NodeSeq) =
+    request.POST.setBody(body.toString) > as.xml.Elem
+  
+  def deleteXML = request.DELETE > as.xml.Elem
+  
+  def createFORM(body: String) = request.POST.setBody(body) > as.xml.Elem
+
+  def download = request.GET > as.Bytes
 }
